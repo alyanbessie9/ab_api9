e67f04a5604764400094d4ec9d6f9d346f38883f
alyanbessie9@gmail.com <alyanbessie9@gmail.com>
2022-12-01 03:38:31 +0700

    first commit

---
+++ get_datatab3.php
@@ -0,0 +1,30 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, Authorization, Accept, X-Requested-With, x-xsrf-token");
+header("Content-Type: application/json; charset=utf-8");
+
+    include "config.php";
+    $data = array();
+
+    $query = mysqli_query($kon, "SELECT * FROM tbl_info order by kd_info");
+
+    while($rows = mysqli_fetch_array($query)){
+        $data[] = array(
+            //'kd_info' => $rows['kd_info'],
+            'info' => $rows['info']
+            
+            //'score' => $rows['score'],
+            //'point' => $rows['point']
+        );
+    }
+
+    if($query){
+        $result = json_encode(array('success'=>true, 'result'=>$data));
+    }else{
+        $result = json_encode(array('success'=>false));
+    }
+    echo $result;
+
+?>
